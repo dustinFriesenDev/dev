3850d08a988e1594635c9de27e5f9b2d6f6e5474
Dustin Dev <94726570+dustinFriesenDev@users.noreply.github.com>
2023-11-08 11:32:46 -0500

    finished adding a joke page and inputs carry over

--- SignUp/Javascript/signUpScript.js
+++ SignUp/Javascript/signUpScript.js
@@ -8,7 +8,7 @@ const nameResponse = [
 ]
 
 contactResponse = [
-  "Here we go...I don't think this is the best way. I'm screaming and I think the police are coming. I'm going to stop.",
+  "Here we go...I don't you hear me. I'm screaming and I think the police are coming. I'm going to stop.",
   "This sounds like fun! Let me go climb a mountain.",
   "I'll look up that the moon and whisper your name in hopes you're looking back thinking of me.",
   "You want me to contact you all three ways? You're demanding a lot!"
@@ -24,12 +24,25 @@ const phoneNumbers = [
 ]
 
 const phoneResponse = [
+  "I love that song! But I know that's not your number!",
   "That's not a real number. What is it really?",
   "What a second...that's my number! I guess I can call you anytime, then.",
   "You should be getting a text from me soon. ;)",
   "I knew it! I tried calling you last week and you didn't pick up."
 ]
 
+const animalResponse = [
+  "My favorite animal is a monkey.",
+  "It's okay to like imaginary creatures. More power to you!"
+  
+]
+
+const birthdayResponse = [
+  "How will I know when to send you a gift?",
+  "(sigh) Me too. Nothing special.",
+  "Wow! You're really smart for a baby. Well done.",
+  "You're a time traveler?! Does this mean I'm my own grandpa?"
+]
 
 function randomIndexRoundDown(max) {
   return Math.floor(Math.random() * max);
@@ -59,11 +72,9 @@ signUp(); //first call to ready the buttons so the buttons don't take two clicks
 function signUp() { // show the form and hide the sign up button.
   var x = document.getElementById("signUp");
   var y = document.getElementById("btnStart");
-  var blur = document.getElementById("blur");
   if (x.style.display === "none") {
     x.style.display = "block";
     y.style.display = "none";
-    blur.style.display = "block";
   } else {
     x.style.display = "none";
   }
@@ -114,19 +125,20 @@ function funForm() { //hide the form and show the results.
     if (selectedValue == "") {
       phoneResults.innerHTML = "What's your number? I've been trying to call you. You won the lottery!"
     } else if (selectedPhone === "867-5309"){
-      phoneResults.innerHTML = `${selectedPhone}` + ": " + "I love that song! But I know that's not your number!"
+      phoneResults.innerHTML = `${selectedPhone}` + ": " + `${phoneResponse[0]}`; 
     } else {
-      phoneResults.innerHTML = `${selectedPhone}` + ": " + `${phoneResponse[randomIndexRoundDown(phoneResponse.length-1)]}`;
+      phoneResults.innerHTML = `${selectedPhone}` + ": " + `${phoneResponse[randomIndexExcludeZero(phoneResponse.length)]}`;
     }
 
   //Animal
     let uAnimal = document.getElementById("animal").value;
     let animalResults = document.getElementById("resultsAnimal");
     if (uAnimal == "") {
-      animalResults.innerHTML = "My favorite animal is a monkey."
+      animalResults.innerHTML = animalResponse[0];
     } else {
-      animalResults.innerHTML = `${uAnimal}` + ": It's okay to like imaginary creatures. More power to you!";
+      animalResults.innerHTML = `${uAnimal}` + ": " + `${animalResponse[randomIndexExcludeZero(animalResponse.length)]}`;
     }
+
   //Birthday
     let birthCheck = document.getElementsByName("birth")
     let birthResults = document.getElementById("resultsBirth");
@@ -134,17 +146,18 @@ function funForm() { //hide the form and show the results.
     for (let i = 0, length = birthCheck.length; i < length; i++) {
       if (birthCheck[i].checked) {
         if (birthCheck[i].id == "past") {
-          birthResults.innerHTML = `${birthCheck[i].value}` + ": (sigh) Me too. Nothing special.";
+          birthResults.innerHTML = `${birthCheck[i].value}` + ": " + birthdayResponse[1];
       } else if (birthCheck[i].id == "present"){
-        birthResults.innerHTML = `${birthCheck[i].value}` + ": Wow! You're really smart for a baby. Well done.";
+        birthResults.innerHTML = `${birthCheck[i].value}` + ": " + birthdayResponse[2];
       } else if(birthCheck[i].id == "future") {
-        birthResults.innerHTML = `${birthCheck[i].value}` + ": You're a time traveler?! Does this mean I'm my own grandpa?";
+        birthResults.innerHTML = `${birthCheck[i].value}` + ": " + birthdayResponse[3];
         } else if (birthCheck[i].id = "noInput" ) {
-          birthResults.innerHTML = "How will I know when to send you a gift?"
+          birthResults.innerHTML = birthdayResponse[0];
         }
       }
     }
   }
+
 backForm(); //first call to ready the buttons so the buttons don't take two clicks.
 function backForm() { //show the form and hide the results.
   let x = document.getElementById("signUp");
@@ -157,10 +170,37 @@ function backForm() { //show the form and hide the results.
     x.style.display = "none";
   }
 }
+readyName();
+function readyName(){
+  uName = document.getElementById("name").value;
+  document.getElementById("jokeReady").innerHTML= "Ready " + uName + "?";
+}
 
-//Results page:
-//grabbing answers from inputs, adding them togethers, and output them to results 
-// var uName = document.getElementById("name");
-// var userInput = uName.value;
-// var response = document.getElementById("resultsName");
-// response.innerHTML = `${userInput}` + ": That's a funny made up name! lol"; 
+toJoke(); //first call to ready the buttons so the buttons don't take two clicks.
+function toJoke() { //show the joke and hide the results.
+  let signup = document.getElementById("signUp");
+  let results = document.getElementById("results");
+  let btnStart = document.getElementById("btnStart");
+  let jokeContainer = document.getElementById('joke-container');
+  if (signup.style.display === "none" && btnStart.style.display === "none" ) {
+    jokeContainer.style.display = "block";
+    signup.style.display = "none";
+    results.style.display = "none";
+  } else {
+    jokeContainer.style.display = "none";
+  }
+}
+
+jokeToResults(); //first call to ready the buttons so the buttons don't take two clicks.
+function jokeToResults() { //show the joke and hide the results.
+  let signup = document.getElementById("signUp");
+  let results = document.getElementById("results");
+  let btnStart = document.getElementById("btnStart");
+  let jokeContainer = document.getElementById('joke-container');
+  if (signup.style.display === "none" && btnStart.style.display === "none" && results.style.display === "none") {
+    jokeContainer.style.display = "none";
+    results.style.display = "block";
+  } else {
+    results.style.display = "none";
+  }
+}
